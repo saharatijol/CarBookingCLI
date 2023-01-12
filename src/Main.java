71bf39e5b44fc7be468c96e8c25be7d2b2a177d7
Sara <sahara.tijol@gmail.com>
2023-01-12 17:54:37 -0600

    UI Menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nWelcome to CLI Car Booking System");
+        System.out.println();
+
+        System.out.println("Select options below: ");
+        System.out.println("1️⃣ - Book Car");
+        System.out.println("2️⃣ - View All User Booked Cars");
+        System.out.println("3️⃣ - View All Bookings");
+        System.out.println("4️⃣ - View Available Cars");
+        System.out.println("5️⃣ - View Available Electric Cars");
+        System.out.println("6️⃣ - View all users");
+        System.out.println("7️⃣ - Exit");
     }
 }
