b8ec050bf41dfe8b5191edd8c756b3fe5a6a839b
Sara <sahara.tijol@gmail.com>
2023-01-12 17:45:07 -0600

    Revert "CLI UI menu"
    This reverts commit 21a897c3b8d53983b30d2874bad843e74395c94b.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\nWelcome to CLI Car Booking System");
-        System.out.println();
-
-        System.out.println("Select options below: ");
-        System.out.println("" +
-                "1️⃣ - Book Car\n" +
-                "2️⃣ - View All User Booked Cars\n" +
-                "3️⃣ - View All Bookings\n" +
-                "4️⃣ - View Available Cars\n" +
-                "5️⃣ - View Available Electric Cars\n" +
-                "6️⃣ - View all users\n" +
-                "7️⃣ - Exit");
+        System.out.println("Hello world!");
     }
 }
